b0efe0412d24256250ae9861ad5aa85d28979b9f
Raphael Barreto <tnt.raphael@gmail.com>
2021-01-05 20:47:45 -0300

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
+/* eslint-disable no-console */
 import React from 'react';
 
-const Home = () => (
-  <div className="home">
-    <h2>Homepage</h2>
-  </div>
-);
+const Home = () => {
+  const handleClick = e => {
+    console.log('Hello Ninja', e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    // console.log(`hello, ${name}, ${e.target}`);
+    console.log('hello', +name, e.target);
+  };
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={e => handleClickAgain('mario', e)}>
+        Click me again!
+      </button>
+    </div>
+  );
+};
 
 export default Home;
