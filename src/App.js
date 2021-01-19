2e2e526db0241b87243412ad29f98579138a4e70
Raphael Barreto <tnt.raphael@gmail.com>
2021-01-19 11:21:13 -0300

    Route Parameters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/create" component={Create} />
+            <Route path="/blogs/:id" component={BlogDetails} />
           </Switch>
         </div>
       </div>
