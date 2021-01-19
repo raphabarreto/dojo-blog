f724f2a56e1e54c506783f9f12e42492dd72f87f
Raphael Barreto <tnt.raphael@gmail.com>
2021-01-19 08:56:06 -0300

    useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -13,18 +13,22 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = id => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   };
 
   useEffect(() => {
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
