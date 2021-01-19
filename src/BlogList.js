2eb174dccc2d31fc0b45bfa21894cb5275545bd3
Raphael Barreto <tnt.raphael@gmail.com>
2021-01-19 09:53:04 -0300

    Fetching Data using useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
-const BlogList = ({ blogs, title, handleDelete }) => {
-  return (
-    <div className="blog-list">
-      <h2>{title}</h2>
-      {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
-        </div>
-      ))}
-    </div>
-  );
-};
+const BlogList = ({ blogs, title }) => (
+  <div className="blog-list">
+    <h2>{title}</h2>
+    {blogs.map(blog => (
+      <div className="blog-preview" key={blog.id}>
+        <h2>{blog.title}</h2>
+        <p>Written by {blog.author}</p>
+      </div>
+    ))}
+  </div>
+);
 
 export default BlogList;
