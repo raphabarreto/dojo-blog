b3d50ef0ce926c5da629b952a1f3623d36214bb3
Raphael Barreto <tnt.raphael@gmail.com>
2021-01-20 19:47:33 -0300

    Making a POST Request

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,22 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
 
     const blog = { title, body, author };
-    console.log(blog);
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    });
   };
 
   return (
@@ -35,7 +45,9 @@ const Create = () => {
 
           <option value="Yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
